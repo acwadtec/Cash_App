2e463c050d2acf591e3966329f019a63551c5c2c
MagedElgawish230 <102017548+MagedElgawish230@users.noreply.github.com>
2025-07-13 22:58:37 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -14,29 +13,6 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'CASH',
-        short_name: 'CASH',
-        start_url: '.',
-        display: 'standalone',
-        background_color: '#18181b',
-        theme_color: '#22d3ee',
-        icons: [
-          {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
   ].filter(Boolean),
   resolve: {
     alias: {
